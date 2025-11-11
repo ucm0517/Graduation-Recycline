02dbc03349ca17c1fda84e86a322acc2e1787bf5
YuChangMin <133028383+ucm0517@users.noreply.github.com>
2025-11-11 22:03:24 +0900

    delete IP address

--- user-ui/src/config.js
+++ user-ui/src/config.js
@@ -1,3 +1,3 @@
 // EC2 서버 주소
-export const SERVER_URL = "http://43.202.10.147:3001";       // EC2 서버 주소
-export const JETSON_URL = "http://192.168.148.244:3002";      // Jetson Nano 서버 주소
+export const SERVER_URL = "http://IP:3001";       // EC2 서버 주소
+export const JETSON_URL = "http://IP:3002";      // Jetson Nano 서버 주소
